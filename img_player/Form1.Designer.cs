8848c733b0e70936a999d6004cf60bc5030efa27
家辉 邓 <919344636@qq.com>
2017-03-17 13:04:54 +0800

    基本功能完成

--- img_player/Form1.Designer.cs
+++ img_player/Form1.Designer.cs
@@ -29,11 +29,192 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Play_Pause = new System.Windows.Forms.Button();
+            this.PgDn = new System.Windows.Forms.Button();
+            this.PgUp = new System.Windows.Forms.Button();
+            this.Fast = new System.Windows.Forms.Button();
+            this.Slow = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.添加文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.play_bar = new System.Windows.Forms.TrackBar();
+            this.Open = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.play_pro = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.play_bar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::img_player.Properties.Resources.无标题;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 244);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Play_Pause
+            // 
+            this.Play_Pause.Location = new System.Drawing.Point(151, 404);
+            this.Play_Pause.Name = "Play_Pause";
+            this.Play_Pause.Size = new System.Drawing.Size(68, 23);
+            this.Play_Pause.TabIndex = 1;
+            this.Play_Pause.Text = "Play";
+            this.Play_Pause.UseVisualStyleBackColor = true;
+            this.Play_Pause.Click += new System.EventHandler(this.Play_Pause_Click);
+            // 
+            // PgDn
+            // 
+            this.PgDn.Location = new System.Drawing.Point(228, 404);
+            this.PgDn.Name = "PgDn";
+            this.PgDn.Size = new System.Drawing.Size(68, 23);
+            this.PgDn.TabIndex = 2;
+            this.PgDn.Text = "PdDn";
+            this.PgDn.UseVisualStyleBackColor = true;
+            this.PgDn.Click += new System.EventHandler(this.PgDn_Click);
+            // 
+            // PgUp
+            // 
+            this.PgUp.Location = new System.Drawing.Point(77, 404);
+            this.PgUp.Name = "PgUp";
+            this.PgUp.Size = new System.Drawing.Size(68, 23);
+            this.PgUp.TabIndex = 3;
+            this.PgUp.Text = "PgUp";
+            this.PgUp.UseVisualStyleBackColor = true;
+            this.PgUp.Click += new System.EventHandler(this.PgUp_Click);
+            // 
+            // Fast
+            // 
+            this.Fast.Location = new System.Drawing.Point(302, 404);
+            this.Fast.Name = "Fast";
+            this.Fast.Size = new System.Drawing.Size(50, 23);
+            this.Fast.TabIndex = 4;
+            this.Fast.Text = ">>";
+            this.Fast.UseVisualStyleBackColor = true;
+            // 
+            // Slow
+            // 
+            this.Slow.Location = new System.Drawing.Point(12, 404);
+            this.Slow.Name = "Slow";
+            this.Slow.Size = new System.Drawing.Size(55, 23);
+            this.Slow.TabIndex = 5;
+            this.Slow.Text = "<<";
+            this.Slow.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(574, 28);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.添加文件ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 添加文件ToolStripMenuItem
+            // 
+            this.添加文件ToolStripMenuItem.Name = "添加文件ToolStripMenuItem";
+            this.添加文件ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.添加文件ToolStripMenuItem.Text = "添加文件";
+            this.添加文件ToolStripMenuItem.Click += new System.EventHandler(this.添加文件ToolStripMenuItem_Click);
+            // 
+            // play_bar
+            // 
+            this.play_bar.Location = new System.Drawing.Point(17, 342);
+            this.play_bar.Name = "play_bar";
+            this.play_bar.Size = new System.Drawing.Size(335, 56);
+            this.play_bar.TabIndex = 8;
+            this.play_bar.Scroll += new System.EventHandler(this.play_bar_Scroll);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(407, 332);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(79, 23);
+            this.Open.TabIndex = 9;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(358, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 244);
+            this.listBox1.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // play_pro
+            // 
+            this.play_pro.AutoSize = true;
+            this.play_pro.Location = new System.Drawing.Point(148, 314);
+            this.play_pro.Name = "play_pro";
+            this.play_pro.Size = new System.Drawing.Size(55, 15);
+            this.play_pro.TabIndex = 11;
+            this.play_pro.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 439);
+            this.Controls.Add(this.play_pro);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.play_bar);
+            this.Controls.Add(this.Slow);
+            this.Controls.Add(this.Fast);
+            this.Controls.Add(this.PgUp);
+            this.Controls.Add(this.PgDn);
+            this.Controls.Add(this.Play_Pause);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.play_bar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Play_Pause;
+        private System.Windows.Forms.Button PgDn;
+        private System.Windows.Forms.Button PgUp;
+        private System.Windows.Forms.Button Fast;
+        private System.Windows.Forms.Button Slow;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 添加文件ToolStripMenuItem;
+        private System.Windows.Forms.TrackBar play_bar;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label play_pro;
     }
 }
 
--- img_player/Form1.cs
+++ img_player/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,210 @@ using System.Windows.Forms;
 
 namespace img_player
 {
+    public struct points
+    {
+        public byte[] img;
+    }
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+        points[] fps = new points[6000];//6000帧数据
+        byte[] buff = new byte[600];//串口读出的图像
+        int[,] map = new int[80, 60];//解压后图像
+        public int time = 0, retime = 0;//记录时间和回放时间
+        List<byte> readList = new List<byte>();//文件读取缓存链表
+        string state = "stop";
+        string[] filenames = new string[100];
+        bool filestrflag = false;
+        private void Play_Pause_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                Play_Pause.Text = "Play";
+                state = "stop";
+                timer1.Enabled = false;
+            }
+            else
+            {
+                Play_Pause.Text = "Pause";
+                state = "play";
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (state == "play")
+            {
+                if (retime < time)
+                {
+                    play(retime);
+                    retime++;
+                }
+            }
+        }
+        void play(int num)
+        {
+            play_bar.Maximum = time;
+            play_bar.Value = retime;
+            play_pro.Text = retime.ToString() + "/" + time.ToString();
+            Changemap(fps[num].img);
+            Display(map);
+        }
+        void Changemap(byte[] imgbuff)
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    for (int k = 0; k < 8; k++)
+                    {
+                        if ((imgbuff[i * 10 + j] & (1 << k)) != 0)
+                        {
+                            map[j * 8 + 7 - k, i] = 1;
+                        }
+                        else
+                        {
+                            map[j * 8 + 7 - k, i] = 0;
+                        }
+                    }
+                }
+            }
+
+        }
+        void Display(int[,] image_buff)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            for (int i = 0; i < 60; i++)
+            {
+                for (int j = 0; j < 80; j++)
+                {
+                    if (image_buff[j, i] == 1)
+                    {
+                        Rectangle rect = new Rectangle(j * 3, i * 3, 3, 3);
+                        g.FillRectangle(Brushes.Black, rect);
+                    }
+                    else
+                    {
+                        Rectangle rect = new Rectangle(j * 3, i * 3, 3, 3);
+                        g.FillRectangle(Brushes.White, rect);
+                    }
+                }
+            }
+        }
+        private void 添加文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opdialog = new OpenFileDialog();
+            opdialog.Multiselect = true;
+            opdialog.Filter = "txt文件|*.txt";
+            DialogResult result = opdialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                for (int i = 0; i < opdialog.FileNames.Length; i++)
+                {
+                    filenames[listBox1.Items.Count] = opdialog.FileNames[i];
+                    listBox1.Items.Add(opdialog.FileNames[i].Substring(opdialog.FileNames[i].LastIndexOf("\\") + 1));
+                }
+            }
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            int n = listBox1.SelectedIndex;
+            // System.Diagnostics.Process.Start(filenames[n]);
+            imgdatainit();
+            try
+            {
+                FileStream file = new FileStream(filenames[n], FileMode.Open);
+                byte[] readByte = new byte[file.Length];
+                file.Seek(0, SeekOrigin.Begin);
+                file.Read(readByte, 0, readByte.Length); //byData传进来的字节数组,用以接受FileStream对象中的数据,第2个参数是字节数组中开始写入数据的位置,它通常是0,表示从数组的开端文件中向数组写数据,最后一个参数规定从文件读多少字符.
+                Read_imgFile(readByte, readByte.Length);
+                file.Close();
+                play(0);
+            }
+            catch (IOException err)
+            {
+                Console.WriteLine(err.ToString());
+            }
+        }
+        void imgdatainit()
+        {
+            for (int i = 0; i < time; i++)
+            {
+                for (int j = 0; j < 600; j++)
+                {
+                    fps[i].img[j] = 0;
+                }
+            }
+            time = 0;
+            retime = 0;
+        }
+        void Read_imgFile(byte[] str, int fpsLength)
+        {
+            fps[time].img = new byte[600];
+            for (int i = 0; i < fpsLength; i++)
+            {
+                if (str[i] == 0xaa && str[i + 1] == 0xbb)
+                    if (str[i + 2] == '*' || str[i + 2] == '$') filestrflag = true;
+                if (filestrflag)
+                    readList.Add(str[i]);
+                if (readList.Count > 3)
+                    if (readList[readList.Count - 1] == 0xee && readList[readList.Count - 2] == 0xdd && readList[readList.Count - 3] == 0xcc)
+                    {
+                        filestrflag = false;
+                        byte[] bytehandler = readList.ToArray();
+                        Readpic(bytehandler, bytehandler.Length - 3);
+                        for (int j = 0; j < 600; j++)
+                        {
+                            fps[time].img[j] = buff[j];
+                        }
+                        readList.Clear();
+                        time++;
+                        fps[time].img = new byte[600];
+                    }
+            }
+        }
+
+        private void play_bar_Scroll(object sender, EventArgs e)
+        {
+            retime = play_bar.Value;
+            if ( retime < time)
+            {
+                play_pro.Text = retime.ToString() + "/" + time.ToString();
+                play(retime);
+            }
+        }
+
+        private void PgUp_Click(object sender, EventArgs e)
+        {
+            if (  retime > 0)
+            {
+                retime--;
+                play(retime);
+            }
+        }
+
+        private void PgDn_Click(object sender, EventArgs e)
+        {
+            if ( retime < time)
+            {
+                retime++;
+                play(retime);
+            }
+        }
+
+        void Readpic(byte[] str, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (i < 600)
+                    buff[i] = str[i + 3];
+            }
         }
     }
 }
